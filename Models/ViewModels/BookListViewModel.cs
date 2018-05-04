d00a939ba0c00f76feec4dd8f4190753842670da
haukur <haukur16@ru.is>
2018-05-04 13:39:17 +0000

    grunnur

---
+++ Models/ViewModels/BookListViewModel.cs
@@ -0,0 +1,13 @@
+namespace BookCave.Models.ViewModels
+{
+  public class BookListViewModel
+  {
+    public int BookId {get; set; }
+    public string Title {get; set; }
+    public string Genre {get; set; }
+    public int ReleseYear {get; set; }
+    public string Author {get; set; }
+    public int AuthorId {get; set; }
+
+  }
+}
