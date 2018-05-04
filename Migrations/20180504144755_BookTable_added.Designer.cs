333bb560573819372b207875072429ef559f8cea
haukur <haukur16@ru.is>
2018-05-04 14:50:27 +0000

    nytt

--- Migrations/20180504144755_BookTable_added.Designer.cs
+++ Migrations/20180504144755_BookTable_added.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace BookCave.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20180504144755_BookTable_added")]
+    partial class BookTable_added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
