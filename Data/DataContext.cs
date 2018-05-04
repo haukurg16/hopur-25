c61baa968a21b4a1105f7b50571d0630392505fc
haukur <haukur16@ru.is>
2018-05-04 14:05:35 +0000

    22

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,7 +6,6 @@ namespace BookCave.Data
   public class DataContext : DbContext
   {
     public DbSet<Author> Authors {get; set; }
-    public DbSet<Book> Books {get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
